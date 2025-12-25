c09c17826750ead593f051b25353768991debf1c
Phuvanenthran-P <phuvan0205@gmail.com>
2025-12-25 18:25:27 +0530

    Project Deployed

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,11 +1,16 @@
 from .base import *
+import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["employee-management-system-ee4i.onrender.com"]
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://employee-management-system-ee4i.onrender.com"
+]
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
