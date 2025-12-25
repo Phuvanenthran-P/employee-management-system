4a5f6c41982860ad891d6912e0252c8bb1c10b44
Phuvanenthran-P <phuvan0205@gmail.com>
2025-12-25 18:54:03 +0530

    changing Django version

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,13 +3,13 @@ import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["employee-management-system-ee4i.onrender.com"]
+ALLOWED_HOSTS = ["employee-management-system-h64u.onrender.com"]
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://employee-management-system-ee4i.onrender.com"
+    "https://employee-management-system-h64u.onrender.com"
 ]
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
