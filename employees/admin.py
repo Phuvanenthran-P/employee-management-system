e65f6327568c3ae7f269849ed12a4e6408f25102
Phuvanenthran-P <phuvan0205@gmail.com>
2025-12-25 09:36:51 +0530

    Updated user permission

--- employees/admin.py
+++ employees/admin.py
@@ -3,4 +3,7 @@ from .models import Employee
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'role', 'salary', 'created_at')
+    list_display = ("id", "name", "role", "salary", "created_at")
+    list_filter = ("role",)
+    search_fields = ("name", "role")
+    ordering = ("-created_at",)
